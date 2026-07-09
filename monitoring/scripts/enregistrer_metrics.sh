18b5f9bebebf17f4ff626bdacac4144f8f639f01
Sparrow <159009604+Sparrow0245@users.noreply.github.com>
2026-07-09 20:19:27 +0200

    Ajout du script d'enregistrement

---
+++ monitoring/scripts/enregistrer_metrics.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+BASE_DIR="$(dirname "$0")"
+
+CONFIG="$BASE_DIR/../config/database.conf"
+
+
+source "$CONFIG"
+
+
+DATA=$("$BASE_DIR/collecte_systeme.sh")
+
+
+CPU=$(echo "$DATA" | cut -d";" -f1)
+RAM=$(echo "$DATA" | cut -d";" -f2)
+DISK=$(echo "$DATA" | cut -d";" -f3)
+LOAD=$(echo "$DATA" | cut -d";" -f4)
+
+
+mysql \
+-u "$DB_USER" \
+-p"$DB_PASSWORD" \
+"$DB_NAME" <<EOF
+
+INSERT INTO metrics
+(
+cpu_usage,
+ram_usage,
+disk_usage,
+load_average
+)
+VALUES
+(
+$CPU,
+$RAM,
+$DISK,
+$LOAD
+);
+
+EOF
