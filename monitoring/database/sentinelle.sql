5db6a7e628fc4608038bb03625a510743a43bc9c
Sparrow <159009604+Sparrow0245@users.noreply.github.com>
2026-07-09 19:10:17 +0200

    Ajout du fichier SQL pour la BDD MySQL du serveur

---
+++ monitoring/database/sentinelle.sql
@@ -0,0 +1,79 @@
+CREATE DATABASE IF NOT EXISTS sentinelle;
+
+USE sentinelle;
+
+
+CREATE TABLE users
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role VARCHAR(20) DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+CREATE TABLE servers
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    hostname VARCHAR(100),
+    ip VARCHAR(50),
+    status VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+CREATE TABLE metrics
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    server_id INT,
+    cpu FLOAT,
+    ram FLOAT,
+    disk FLOAT,
+    uptime VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY(server_id)
+    REFERENCES servers(id)
+);
+
+
+
+CREATE TABLE alerts
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    severity VARCHAR(20),
+    message TEXT,
+
+    status VARCHAR(20)
+    DEFAULT 'NEW',
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+CREATE TABLE events
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    type VARCHAR(50),
+
+    message TEXT,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+INSERT INTO servers
+(hostname,ip,status)
+VALUES
+(
+"localhost",
+"127.0.0.1",
+"ACTIVE"
+);
