1dd7e16d7f139a38a6a5cc13c8fbf2edbf3b4ef4
Sparrow <159009604+Sparrow0245@users.noreply.github.com>
2026-07-29 14:33:56 +0200

    Ajout du fichier App.vue

---
+++ monitoring/04_sentinelle_supervision_securisee/frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #1a1a1a;
+  color: #ffffff;
+}
+</style>
