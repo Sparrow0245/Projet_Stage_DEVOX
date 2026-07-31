778b9a4f25dad117d76af293b2a2003c05c056cc
Sparrow <159009604+Sparrow0245@users.noreply.github.com>
2026-07-31 17:05:17 +0200

    Correction du script collect_disk.sh

--- monitoring/04_sentinelle_supervision_securisee/bash/collect/collect_disk.sh
+++ monitoring/04_sentinelle_supervision_securisee/bash/collect/collect_disk.sh
@@ -10,11 +10,11 @@ DB_CNF="/etc/mysql/sentinelle.cnf"
 DISK_USAGE=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 
 mysql --defaults-extra-file="${DB_CNF}" sentinelle -e \
-"INSERT INTO metrics (host_id, cpu_usage, ram_usage, disk_usage, swap_usage, network_rx_kb, network_tx_kb) \
-VALUES (1, 0, 0, ${DISK_USAGE}, 0, 0, 0);"
+"INSERT INTO metrics (host_id, cpu_usage, ram_usage, disk_usage, swap_usage) \
+VALUES (1, 0, 0, ${DISK_USAGE}, 0);"
 
 if (( $(echo "${DISK_USAGE} > 85" | bc -l) )); then
     mysql --defaults-extra-file="${DB_CNF}" sentinelle -e \
-    "INSERT INTO events (host_id, event_type, severity, message) \
-    VALUES (1, 'DISK_HIGH', 'WARNING', 'Partition racine saturée à ${DISK_USAGE}%');"
+    "INSERT INTO events (host_id, type, message) \
+    VALUES (1, 'WARNING', 'Partition racine saturée à ${DISK_USAGE}%');"
 fi
