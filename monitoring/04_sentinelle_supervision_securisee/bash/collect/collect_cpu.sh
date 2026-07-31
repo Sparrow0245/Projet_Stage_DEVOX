de3cb8abd1ad6390aac08d867a0515be6fc63aaa
Sparrow <159009604+Sparrow0245@users.noreply.github.com>
2026-07-31 17:02:55 +0200

    Correction du fichier collect_cpu.sh

--- monitoring/04_sentinelle_supervision_securisee/bash/collect/collect_cpu.sh
+++ monitoring/04_sentinelle_supervision_securisee/bash/collect/collect_cpu.sh
@@ -12,12 +12,12 @@ CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk
 
 # Insertion de la métrique
 mysql --defaults-extra-file="${DB_CNF}" sentinelle -e \
-"INSERT INTO metrics (host_id, cpu_usage, ram_usage, disk_usage, swap_usage, network_rx_kb, network_tx_kb) \
-VALUES (1, ${CPU_USAGE}, 0, 0, 0, 0, 0);"
+"INSERT INTO metrics (host_id, cpu_usage, ram_usage, disk_usage, swap_usage) \
+VALUES (1, ${CPU_USAGE}, 0, 0, 0);"
 
 # Contrôle du seuil d'alerte (Ex: > 80%)
 if (( $(echo "${CPU_USAGE} > 80.0" | bc -l) )); then
     mysql --defaults-extra-file="${DB_CNF}" sentinelle -e \
-    "INSERT INTO events (host_id, event_type, severity, message) \
-    VALUES (1, 'CPU_HIGH', 'WARNING', 'Consommation CPU élevée : ${CPU_USAGE}%');"
+    "INSERT INTO events (host_id, type, message) \
+    VALUES (1, 'WARNING', 'Consommation CPU élevée : ${CPU_USAGE}%');"
 fi
