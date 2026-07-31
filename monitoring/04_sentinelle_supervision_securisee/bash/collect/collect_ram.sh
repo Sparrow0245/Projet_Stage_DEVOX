1342f284fadc28b604c503fbf8b015b717450632
Sparrow <159009604+Sparrow0245@users.noreply.github.com>
2026-07-31 17:08:21 +0200

    Correction du script collect_ram.sh

--- monitoring/04_sentinelle_supervision_securisee/bash/collect/collect_ram.sh
+++ monitoring/04_sentinelle_supervision_securisee/bash/collect/collect_ram.sh
@@ -11,11 +11,11 @@ RAM_USAGE=$(free -m | awk '/Mem:/ { printf("%.2f", $3/$2 * 100) }')
 SWAP_USAGE=$(free -m | awk '/Swap:/ { if ($2>0) printf("%.2f", $3/$2 * 100); else print 0.0 }')
 
 mysql --defaults-extra-file="${DB_CNF}" sentinelle -e \
-"INSERT INTO metrics (host_id, cpu_usage, ram_usage, disk_usage, swap_usage, network_rx_kb, network_tx_kb) \
-VALUES (1, 0, ${RAM_USAGE}, 0, ${SWAP_USAGE}, 0, 0);"
+"INSERT INTO metrics (host_id, cpu_usage, ram_usage, disk_usage, swap_usage) \
+VALUES (1, 0, ${RAM_USAGE}, 0, ${SWAP_USAGE});"
 
 if (( $(echo "${RAM_USAGE} > 85.0" | bc -l) )); then
     mysql --defaults-extra-file="${DB_CNF}" sentinelle -e \
-    "INSERT INTO events (host_id, event_type, severity, message) \
-    VALUES (1, 'RAM_HIGH', 'WARNING', 'Consommation RAM élevée : ${RAM_USAGE}%');"
+    "INSERT INTO events (host_id, type, message) \
+    VALUES (1, 'WARNING', 'Consommation RAM élevée : ${RAM_USAGE}%');"
 fi
