ec67cb16289119b836636e23d08f8584f51f6e8e
Sparrow <159009604+Sparrow0245@users.noreply.github.com>
2026-08-01 16:25:27 +0200

    Correction du nom de fichier

--- monitoring/04_sentinelle_supervision_securisee/backend/src/main/java/com/sentinelle/service/AlertesService.java
+++ monitoring/04_sentinelle_supervision_securisee/backend/src/main/java/com/sentinelle/service/AlertesService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AlerteService {
+public class AlertsService {
 
     @Autowired
     private AlertsRepository alertsRepository;
