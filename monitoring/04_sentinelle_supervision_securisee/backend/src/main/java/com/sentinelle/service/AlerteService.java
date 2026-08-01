ee477a89aaa2db37c8a6d16d593f446f561c2eb3
Sparrow <159009604+Sparrow0245@users.noreply.github.com>
2026-08-01 15:54:06 +0200

    Correction du fichier AlerteService.java

--- monitoring/04_sentinelle_supervision_securisee/backend/src/main/java/com/sentinelle/service/AlerteService.java
+++ monitoring/04_sentinelle_supervision_securisee/backend/src/main/java/com/sentinelle/service/AlerteService.java
@@ -1,18 +1,22 @@
 package com.sentinelle.service;
 
-import com.sentinelle.repository.AlertsRepository;
 import com.sentinelle.model.Alert;
+import com.sentinelle.repository.AlertsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AlertsService {
+public class AlerteService {
 
     @Autowired
     private AlertsRepository alertsRepository;
 
-    public List<Alert> getActiveAlerts() {
-        return alertsRepository.findByStatus("NEW");
+    public List<Alert> getAllAlerts() {
+        return alertsRepository.findAll();
+    }
+
+    public Alert saveAlert(Alert alert) {
+        return alertsRepository.save(alert);
     }
 }
